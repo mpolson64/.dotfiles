b61cc320c74af5e27aa30e24b6c150ce7cc2c7f0
Miles P. Olson <mpolson64@gmail.com>
2016-05-07 15:16:39 -0700

    Made install clone vundle and install bundles on run

--- install.sh
+++ install.sh
@@ -2,5 +2,9 @@
 ln -sfv ~/.dotfiles/git/.gitconfig ~
 
 # Vim
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
 ln -sfv ~/.dotfiles/vim/.vimrc ~
 ln -sfv ~/.dotfiles/vim/vundle.vim ~/.vim
+
+vim +PluginInstall +qall
