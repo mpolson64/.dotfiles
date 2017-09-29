9f9873a64631383184fd54e9fe41920d3ee67312
Miles P. Olson <mpolson64@gmail.com>
2017-09-29 11:39:53 -0500

    Install now makes directories and files needed

--- install.sh
+++ install.sh
@@ -1,12 +1,17 @@
 ## Git
+touch ~/.gitconfig
 ln -sfv ~/.dotfiles/git/.gitconfig ~
+
+touch ~/.gitignore
 ln -sfv ~/.dotfiles/git/.gitignore ~
 
 ## Vim
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
+touch ~/.vimrc
 ln -sfv ~/.dotfiles/vim/.vimrc ~
-mkdir ~/.vim
+
+mkdir -p ~/.vim
 ln -sfv ~/.dotfiles/vim/vundle.vim ~/.vim
 
 vim +PluginInstall +qall
@@ -15,9 +20,13 @@ vim +PluginInstall +qall
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 pip3 install neovim
 
+mkdir -p ~/.config/nvim
+touch ~/.config/nvim/init.vim
 ln -sfv ~/.dotfiles/config/nvim/init.vim ~/.config/nvim/init.vim
 
 nvim +PlugInstall +qall
 
 ## Emacs
+mkdir -p ~/.emacs.d
+touch ~/.emacs.d/init.el
 ln -sfv ~/.dotfiles/emacs.d/init.el ~/.emacs.d/init.el
