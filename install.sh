f073482c44877c5118101ed2fee74ca22021ff95
Miles P. Olson <mpolson64@gmail.com>
2017-10-01 16:28:29 -0500

    Added prompts to skip installation of any set of dotfiles

--- install.sh
+++ install.sh
@@ -1,32 +1,90 @@
 ## Git
-touch ~/.gitconfig
-ln -sfv ~/.dotfiles/git/.gitconfig ~
+while true; do
+    read -p "Configure git? [Y/n]" yn
+    case $yn in
+        [Yy]* )
+            touch ~/.gitconfig
+            ln -sfv ~/.dotfiles/git/.gitconfig ~
 
-touch ~/.gitignore
-ln -sfv ~/.dotfiles/git/.gitignore ~
+            touch ~/.gitignore
+            ln -sfv ~/.dotfiles/git/.gitignore ~
+            break;;
+        [Nn]* )
+            echo "Git skipped"
+            break;;
+        * )
+            echo "Please answer y or n"
+    esac
+done
 
 ## Vim
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+while true; do
+    read -p "Configure vim? [Y/n]" yn
+    case $yn in
+        [Yy]* )
+            git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
-touch ~/.vimrc
-ln -sfv ~/.dotfiles/vim/.vimrc ~
+            touch ~/.vimrc
+            ln -sfv ~/.dotfiles/vim/.vimrc ~
 
-mkdir -p ~/.vim
-ln -sfv ~/.dotfiles/vim/vundle.vim ~/.vim
+            mkdir -p ~/.vim
+            ln -sfv ~/.dotfiles/vim/vundle.vim ~/.vim
 
-vim +PluginInstall +qall
+            vim +PluginInstall +qall
+
+            break;;
+
+        [Nn]* )
+            echo "Vim skipped"
+            break;;
+
+        * )
+            echo "Please answer y or n"
+    esac
+done
 
 ## Neovim
-curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-pip3 install neovim
+while true; do
+    read -p "Configure neovim? [Y/n]" yn
+    case $yn in
+        [Yy]* )
+            curl -flo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+            pip3 install neovim
 
-mkdir -p ~/.config/nvim
-touch ~/.config/nvim/init.vim
-ln -sfv ~/.dotfiles/config/nvim/init.vim ~/.config/nvim/init.vim
+            mkdir -p ~/.config/nvim
+            touch ~/.config/nvim/init.vim
+            ln -sfv ~/.dotfiles/config/nvim/init.vim ~/.config/nvim/init.vim
 
-nvim +PlugInstall +qall
+            nvim +PlugInstall +qall
+            break;;
+
+        [Nn]* )
+            echo "Neovim skipped"
+            break;;
+
+        * )
+            echo "Please answer y or n"
+    esac
+done
 
 ## Emacs
-mkdir -p ~/.emacs.d
-touch ~/.emacs.d/init.el
-ln -sfv ~/.dotfiles/emacs.d/init.el ~/.emacs.d/init.el
+while true; do
+    read -p "Configure EMACS? [Y/n]" yn
+    case $yn in
+        [Yy]* )
+            mkdir -p ~/.emacs.emacs
+            touch ~/.emacs.d/init.el
+            ln -sfv ~/.dotfiles/emacs.d/init.el ~/.emacs.d/init.el
+            break;;
+
+        [Nn]* )
+            echo "EMACS skipped"
+            break;;
+
+        * )
+            echo "Please answer y or n"
+    esac
+done
+
+
+exit
